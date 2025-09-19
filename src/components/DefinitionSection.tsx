19918c0156eb3d4c08b06c8b34f54e022be1169f
noahevola <luhrsnoah@gmail.com>
2025-09-19 23:11:09 +0100

    Update DefinitionSection.tsx

--- src/components/DefinitionSection.tsx
+++ src/components/DefinitionSection.tsx
@@ -39,7 +39,7 @@ function DefinitionSection() {
         <div className="flex justify-center mt-8">
           <button 
             onClick={scrollToForm}
-            className="bg-[#FFF041] text-black font-black text-sm sm:text-base md:text-lg lg:text-xl px-4 sm:px-6 md:px-8 py-3 md:py-4 rounded-lg hover:bg-[#E6D93A] transition-colors duration-200 flex items-center justify-center space-x-2 shadow-[0_0_60px_rgba(255,255,255,0.6)] w-fit"
+            className="bg-[#FFF041] text-black font-bold text-sm sm:text-base md:text-lg lg:text-xl px-4 sm:px-6 md:px-8 py-3 md:py-4 rounded-lg hover:bg-[#E6D93A] transition-colors duration-200 flex items-center justify-center space-x-2 shadow-[0_0_60px_rgba(255,255,255,0.6)] w-fit"
           >
             <span>Why Is It Broken, And How Can I Fix It?</span>
             <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
