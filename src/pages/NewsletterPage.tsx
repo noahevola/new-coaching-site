9fc7a18988ff39dcf5539cfbd6f2a30c8b2cc591
noahevola <luhrsnoah@gmail.com>
2025-09-20 13:28:41 +0100

    Update NewsletterPage.tsx

--- src/pages/NewsletterPage.tsx
+++ src/pages/NewsletterPage.tsx
@@ -10,13 +10,8 @@ function NewsletterPage() {
       <Header showApplyButton={true} />
       
       <div className="bg-black text-white min-h-screen flex flex-col items-center justify-start py-12 px-4">
-        <h1 className="text-4xl md:text-5xl font-bold text-center mb-12">
-          Join The Daily Dose
-        </h1>
-
         {/* Embedded newsletter form */}
         <Newsletter embedded={true} />
-
       </div>
 
       <Footer />
