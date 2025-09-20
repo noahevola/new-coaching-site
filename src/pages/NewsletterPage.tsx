1aeae23c7287e280f3a369a4a36e334baea58349
noahevola <luhrsnoah@gmail.com>
2025-09-20 14:10:25 +0100

    Update NewsletterPage.tsx

--- src/pages/NewsletterPage.tsx
+++ src/pages/NewsletterPage.tsx
@@ -6,16 +6,26 @@ import Footer from '../components/Footer';
 
 function NewsletterPage() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen bg-black text-white">
       <Header showApplyButton={true} />
-      
-      <div className="bg-black text-white min-h-screen flex flex-col items-center justify-center py-12 px-4">
-        {/* Embedded newsletter form */}
+
+      {/* Heading & Subheading */}
+      <div className="text-center mt-8 px-4">
+        <h3 className="text-2xl sm:text-3xl md:text-4xl font-black">
+          <span className="bg-[#FFF041] text-black px-2 py-1">Join My Free Newsletter</span>
+        </h3>
+        <p className="mt-3 text-lg font-semibold">
+          Daily wisdom to speedrun trading psychology
+        </p>
+      </div>
+
+      {/* Newsletter Form */}
+      <div className="flex-grow flex items-center justify-center px-4 py-12">
         <Newsletter embedded={true} />
       </div>
 
       <Footer />
-    </>
+    </div>
   );
 }
 
